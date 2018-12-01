61c2a289623e8518a277e0fb2ddfa2cd1efc293a
Hisashi Nakamura <hisashi@sunvisor.net>
2018-12-01 09:33:40 +0900

    - 表示する天気予報を14時間後2時間ごとに変更 - 予定の表示を大きくした - 土日の場合に日付に色を付ける

--- client/app/view/main/MainController.js
+++ client/app/view/main/MainController.js
@@ -19,29 +19,31 @@ Ext.define('HomeDashboard.view.main.MainController', {
     },
 
     WEEK_NAMES: '日月火水木金土',
+    WEEK_CLASS: ['sunday', '', '', '', '', '', 'saturday'],
 
     onLoadWeather: function (view, weather) {
         let hourly = [];
 
-        for(let i=0; i < 24; i++) {
-            if (((i + 1)%4) === 0) {
+        for (let i = 0; i < 14; i++) {
+            if (((i + 1) % 2) === 0) {
                 hourly.push(weather.hourly.data[i]);
             }
         }
         const today = new Date();
 
         view.down('weather').setData({
-            month: today.getMonth() + 1,
-            day: today.getDate(),
-            dayOfTheWeek: this.WEEK_NAMES[today.getDay()],
-            temperature: weather.currently.temperature,
-            icon: weather.currently.icon,
-            temperatureLow: weather.daily.data[0].temperatureLow,
-            temperatureHigh: weather.daily.data[0].temperatureHigh,
-            precipIntensity: weather.daily.data[0].precipIntensity,
+            month            : today.getMonth() + 1,
+            day              : today.getDate(),
+            dayOfTheWeek     : this.WEEK_NAMES[today.getDay()],
+            week             : this.WEEK_CLASS[today.getDay()],
+            temperature      : weather.currently.temperature,
+            icon             : weather.currently.icon,
+            temperatureLow   : weather.daily.data[0].temperatureLow,
+            temperatureHigh  : weather.daily.data[0].temperatureHigh,
+            precipIntensity  : weather.daily.data[0].precipIntensity,
             precipProbability: weather.daily.data[0].precipProbability,
-            windSpeed: weather.currently.windSpeed,
-            pressure: weather.currently.pressure,
+            windSpeed        : weather.currently.windSpeed,
+            pressure         : weather.currently.pressure,
             hourly
         });
     },
